0460f2ea5bd2505054a1e9ac00f19e5b1fd8e417
Maarten Aerts <maarten.702@gmail.com>
2014-07-03 17:16:32 +0200

    Mongo db

--- Heart_Fat/settings.py
+++ Heart_Fat/settings.py
@@ -58,10 +58,11 @@ WSGI_APPLICATION = 'Heart_Fat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django_mongodb_engine',
+        'NAME': 'HeartDb'
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
