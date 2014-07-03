dfd8d4cf50c0b574bed7ef38e992783aded87c67
Savor d'Isavano <newelevenken@aliyun.com>
2014-07-04 00:54:13 +0800

    the first view. works!

--- Heart_Fat/urls.py
+++ Heart_Fat/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.views.generic import TemplateView
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'Heart_Fat.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', TemplateView.as_view(template_name='Heart_Fat_App/index.html')),
     url(r'^admin/', include(admin.site.urls)),
 )
---
+++ Heart_Fat_App/urls.py
@@ -0,0 +1,6 @@
+__author__ = 'kj'
+
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns(''
+)
